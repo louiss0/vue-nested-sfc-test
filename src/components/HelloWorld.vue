02159d4119e3688d1706f42c607470797c2ea830
Shelton Louis <louisshelton0@gmail.com>
2023-05-05 16:15:48 -0400

    Finished playing with demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,46 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script setup lang="tsx">
+import { FunctionalComponent, ref } from 'vue'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+ 
+
+
+type Events = { alertText(word: string): void; onThis(): string;  }
+
+type ObjectThatRejectsOn<T extends Record<string, unknown>> = Omit<T,`on${string}`>   
+
+type CreateObjectPrefixedWithOnAndCapitalized<T extends Record<string, unknown>> = {
+  [K in keyof ObjectThatRejectsOn<T> as `on${Capitalize<K & string>}`]:ObjectThatRejectsOn<T>[K]
+}
+type ObjectWithOnlyFunctions = Record<string, (...args: Array<any>) => any>; 
+
+type ProperFunctionalComponent< T extends Record<string, unknown>, U extends ObjectWithOnlyFunctions> =
+  FunctionalComponent<
+    Omit<T, keyof U> & CreateObjectPrefixedWithOnAndCapitalized<U>,
+    ObjectThatRejectsOn<U>
+  >
+
+
+const Example: ProperFunctionalComponent<{ text: string;  }, Events> = (props, { emit }) => (
+  <button onClick={() => emit("alertText", props.text)} >
+    Click to get a text alert 
+  </button>
+)
+
+Example.props = ['text']
+
+Example.emits = ["alertText"]
+
+
 </script>
 
 
 <template>
   <h1>{{ msg }}</h1>
-  <Message />
-
+  <Message word="nice" />
+<Example text=""  @alertText=""   />
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
@@ -19,6 +49,7 @@ const count = ref(0)
     </p>
   </div>
 
+  <input type="text">
   <p>
     Check out
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
@@ -34,11 +65,39 @@ const count = ref(0)
 </template>
 
 <component name="Message" lang="html" >
-  <template>
-  this is a message
-  </template>
   <script setup lang="ts"> 
+  
+  const message = "This is a message"
+
+  defineProps({
+    word:{
+      type:String,
+      required:true,
+      validator:(value:string)=> {
+          const message = "This is a message"
+
+          const listofWordsInMessage = message.toLowerCase().split(/\s/)
+
+          if(listofWordsInMessage.includes(value.toLowerCase())) {
+
+            throw new Error(`
+            The word is not suppossed to be ${listofWordsInMessage.join(",")}.
+            They make up the message. 
+            `)
+          }
+
+          return true
+
+      } 
+    }
+  })
+
   </script>
+  <template>
+    <button>
+      {{ message }} {{ word }}
+    </button>
+  </template>
 </component>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueNestedSfc from 'vite-plugin-vue-nested-sfc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueNestedSfc()],
+  plugins: [vue(), vueJsx({ enableObjectSlots: true }), vueNestedSfc()],
 })
