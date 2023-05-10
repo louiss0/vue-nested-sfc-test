fe61e6cbd05d051f2dacb55d2b33dee833dc8e0c
louiss0 <louisshelton0@gmail.com>
2023-05-10 14:17:42 -0400

    Changed lang = vue to lang = html

--- src/components/ComponentSets.vue
+++ src/components/ComponentSets.vue
@@ -1,10 +1,10 @@
 
 <template class=""></template>
 
-<component export name="text-one" >
+<component export name="text-one" lang="html" >
     <template>This is text one</template>
 </component>
 
-<component export name="text-two" >
+<component export name="text-two" lang="html">
     <template>This is text two.</template>
 </component>
